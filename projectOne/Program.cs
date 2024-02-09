d31872846bde51a5a5360858090af27be9c469aa
gergoke68 <gemes.gergo2007@gmail.com>
2024-02-09 12:48:51 +0100

    Update

--- projectOne/Program.cs
+++ projectOne/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System.Drawing;
+using System.Media;
 
 Menu();
 string playerName = "";
 bool isMiloWithUs = false;
+int playerHealth = 100;
+int playerStrenght = 1;
+
+string[] enemyNames = { "Gorlock", "Maru", "Bober", "Pingwin", "Bob", "József", "Fereng", "Vojtech", "Togepi", "Kirby"};
+
 void Menu()
 {
 
-Console.WriteLine("\r\n ('-. .-.   ('-.                      .-') _  .-') _                                        .-') _    ('-. .-.   ('-.         _ .-') _     ('-.     _  .-')  .-. .-')           ('-.                   ('-.    .-')    \r\n( OO )  /  ( OO ).-.                 ( OO ) )(  OO) )                                      (  OO) )  ( OO )  / _(  OO)       ( (  OO) )   ( OO ).-.( \\( -O ) \\  ( OO )         ( OO ).-.             _(  OO)  ( OO ).  \r\n,--. ,--.  / . --. / ,--. ,--.   ,--./ ,--,' /     '._        .-'),-----.    ,------.      /     '._ ,--. ,--.(,------.       \\     .'_   / . --. / ,------. ,--. ,--.         / . --. /  ,----.    (,------.(_)---\\_) \r\n|  | |  |  | \\-.  \\  |  | |  |   |   \\ |  |\\ |'--...__)      ( OO'  .-.  '('-| _.---'      |'--...__)|  | |  | |  .---'       ,`'--..._)  | \\-.  \\  |   /`. '|  .'   /         | \\-.  \\  '  .-./-')  |  .---'/    _ |  \r\n|   .|  |.-'-'  |  | |  | | .-') |    \\|  | )'--.  .--'      /   |  | |  |(OO|(_\\          '--.  .--'|   .|  | |  |           |  |  \\  '.-'-'  |  | |  /  | ||      /,       .-'-'  |  | |  |_( O- ) |  |    \\  :` `.  \r\n|       | \\| |_.'  | |  |_|( OO )|  .     |/    |  |         \\_) |  |\\|  |/  |  '--.          |  |   |       |(|  '--.        |  |   ' | \\| |_.'  | |  |_.' ||     ' _)       \\| |_.'  | |  | .--, \\(|  '--.  '..`''.) \r\n|  .-.  |  |  .-.  | |  | | `-' /|  |\\    |     |  |           \\ |  | |  |\\_)|  .--'          |  |   |  .-.  | |  .--'        |  |   / :  |  .-.  | |  .  '.'|  .   \\          |  .-.  |(|  | '. (_/ |  .--' .-._)   \\ \r\n|  | |  |  |  | |  |('  '-'(_.-' |  | \\   |     |  |            `'  '-'  '  \\|  |_)           |  |   |  | |  | |  `---.       |  '--'  /  |  | |  | |  |\\  \\ |  |\\   \\         |  | |  | |  '--'  |  |  `---.\\       / \r\n`--' `--'  `--' `--'  `-----'    `--'  `--'     `--'              `-----'    `--'             `--'   `--' `--' `------'       `-------'   `--' `--' `--' '--'`--' '--'         `--' `--'  `------'   `------' `-----'  \r\n");
+Print("\r\n ('-. .-.   ('-.                      .-') _  .-') _                                        .-') _    ('-. .-.   ('-.         _ .-') _     ('-.     _  .-')  .-. .-')           ('-.                   ('-.    .-')    \r\n( OO )  /  ( OO ).-.                 ( OO ) )(  OO) )                                      (  OO) )  ( OO )  / _(  OO)       ( (  OO) )   ( OO ).-.( \\( -O ) \\  ( OO )         ( OO ).-.             _(  OO)  ( OO ).  \r\n,--. ,--.  / . --. / ,--. ,--.   ,--./ ,--,' /     '._        .-'),-----.    ,------.      /     '._ ,--. ,--.(,------.       \\     .'_   / . --. / ,------. ,--. ,--.         / . --. /  ,----.    (,------.(_)---\\_) \r\n|  | |  |  | \\-.  \\  |  | |  |   |   \\ |  |\\ |'--...__)      ( OO'  .-.  '('-| _.---'      |'--...__)|  | |  | |  .---'       ,`'--..._)  | \\-.  \\  |   /`. '|  .'   /         | \\-.  \\  '  .-./-')  |  .---'/    _ |  \r\n|   .|  |.-'-'  |  | |  | | .-') |    \\|  | )'--.  .--'      /   |  | |  |(OO|(_\\          '--.  .--'|   .|  | |  |           |  |  \\  '.-'-'  |  | |  /  | ||      /,       .-'-'  |  | |  |_( O- ) |  |    \\  :` `.  \r\n|       | \\| |_.'  | |  |_|( OO )|  .     |/    |  |         \\_) |  |\\|  |/  |  '--.          |  |   |       |(|  '--.        |  |   ' | \\| |_.'  | |  |_.' ||     ' _)       \\| |_.'  | |  | .--, \\(|  '--.  '..`''.) \r\n|  .-.  |  |  .-.  | |  | | `-' /|  |\\    |     |  |           \\ |  | |  |\\_)|  .--'          |  |   |  .-.  | |  .--'        |  |   / :  |  .-.  | |  .  '.'|  .   \\          |  .-.  |(|  | '. (_/ |  .--' .-._)   \\ \r\n|  | |  |  |  | |  |('  '-'(_.-' |  | \\   |     |  |            `'  '-'  '  \\|  |_)           |  |   |  | |  | |  `---.       |  '--'  /  |  | |  | |  |\\  \\ |  |\\   \\         |  | |  | |  '--'  |  |  `---.\\       / \r\n`--' `--'  `--' `--'  `-----'    `--'  `--'     `--'              `-----'    `--'             `--'   `--' `--' `------'       `-------'   `--' `--' `--' '--'`--' '--'         `--' `--'  `------'   `------' `-----'  \r\n", 0);
 
-Console.WriteLine("\r\n  __              _____            _   _                                   \r\n /_ |            / ____|          | | (_)                                  \r\n  | |  ______   | |     ___  _ __ | |_ _ _ __  _   _  ___                  \r\n  | | |______|  | |    / _ \\| '_ \\| __| | '_ \\| | | |/ _ \\                 \r\n  | |           | |___| (_) | | | | |_| | | | | |_| |  __/                 \r\n  |_|            \\_____\\___/|_| |_|\\__|_|_| |_|\\__,_|\\___|                 \r\n |__ \\            | \\ | |                                                  \r\n    ) |  ______   |  \\| | _____      __   __ _  __ _ _ __ ___   ___        \r\n   / /  |______|  | . ` |/ _ \\ \\ /\\ / /  / _` |/ _` | '_ ` _ \\ / _ \\       \r\n  / /_            | |\\  |  __/\\ V  V /  | (_| | (_| | | | | | |  __/       \r\n |____|           |_| \\_|\\___| \\_/\\_/    \\__, |\\__,_|_| |_| |_|\\___|       \r\n |___ \\            | \\ | |                __/ |                        _   \r\n   __) |  ______   |  \\| | _____      __ |___/_  __ _ _ __ ___   ___ _| |_ \r\n  |__ <  |______|  | . ` |/ _ \\ \\ /\\ / /  / _` |/ _` | '_ ` _ \\ / _ \\_   _|\r\n  ___) |           | |\\  |  __/\\ V  V /  | (_| | (_| | | | | | |  __/ |_|  \r\n |____/            |_| \\_|\\___| \\_/\\_/    \\__, |\\__,_|_| |_| |_|\\___|      \r\n  _  _                _____      _   _   _ __/ |                           \r\n | || |              / ____|    | | | | (_)___/                            \r\n | || |_   ______   | (___   ___| |_| |_ _ _ __   __ _ ___                 \r\n |__   _| |______|   \\___ \\ / _ \\ __| __| | '_ \\ / _` / __|                \r\n    | |              ____) |  __/ |_| |_| | | | | (_| \\__ \\                \r\n  __|_|             |_____/ \\___|\\__|\\__|_|_| |_|\\__, |___/                \r\n | ____|            / ____|            | (_) |    __/ |                    \r\n | |__    ______   | |     _ __ ___  __| |_| |_ _|___/                     \r\n |___ \\  |______|  | |    | '__/ _ \\/ _` | | __/ __|                       \r\n  ___) |           | |____| | |  __/ (_| | | |_\\__ \\                       \r\n |____/             \\_____|_|  \\___|\\__,_|_|\\__|___/                       \r\n   / /             |  ____|    (_) |                                       \r\n  / /_    ______   | |__  __  ___| |_                                      \r\n | '_ \\  |______|  |  __| \\ \\/ / | __|                                     \r\n | (_) |           | |____ >  <| | |_                                      \r\n  \\___/            |______/_/\\_\\_|\\__|                                     \r\n                                                                           \r\n                                                                           \r\n");
+Print("\r\n  __              _____            _   _                                   \r\n /_ |            / ____|          | | (_)                                  \r\n  | |  ______   | |     ___  _ __ | |_ _ _ __  _   _  ___                  \r\n  | | |______|  | |    / _ \\| '_ \\| __| | '_ \\| | | |/ _ \\                 \r\n  | |           | |___| (_) | | | | |_| | | | | |_| |  __/                 \r\n  |_|            \\_____\\___/|_| |_|\\__|_|_| |_|\\__,_|\\___|                 \r\n |__ \\            | \\ | |                                                  \r\n    ) |  ______   |  \\| | _____      __   __ _  __ _ _ __ ___   ___        \r\n   / /  |______|  | . ` |/ _ \\ \\ /\\ / /  / _` |/ _` | '_ ` _ \\ / _ \\       \r\n  / /_            | |\\  |  __/\\ V  V /  | (_| | (_| | | | | | |  __/       \r\n |____|           |_| \\_|\\___| \\_/\\_/    \\__, |\\__,_|_| |_| |_|\\___|       \r\n |___ \\            | \\ | |                __/ |                        _   \r\n   __) |  ______   |  \\| | _____      __ |___/_  __ _ _ __ ___   ___ _| |_ \r\n  |__ <  |______|  | . ` |/ _ \\ \\ /\\ / /  / _` |/ _` | '_ ` _ \\ / _ \\_   _|\r\n  ___) |           | |\\  |  __/\\ V  V /  | (_| | (_| | | | | | |  __/ |_|  \r\n |____/            |_| \\_|\\___| \\_/\\_/    \\__, |\\__,_|_| |_| |_|\\___|      \r\n  _  _                _____      _   _   _ __/ |                           \r\n | || |              / ____|    | | | | (_)___/                            \r\n | || |_   ______   | (___   ___| |_| |_ _ _ __   __ _ ___                 \r\n |__   _| |______|   \\___ \\ / _ \\ __| __| | '_ \\ / _` / __|                \r\n    | |              ____) |  __/ |_| |_| | | | | (_| \\__ \\                \r\n  __|_|             |_____/ \\___|\\__|\\__|_|_| |_|\\__, |___/                \r\n | ____|            / ____|            | (_) |    __/ |                    \r\n | |__    ______   | |     _ __ ___  __| |_| |_ _|___/                     \r\n |___ \\  |______|  | |    | '__/ _ \\/ _` | | __/ __|                       \r\n  ___) |           | |____| | |  __/ (_| | | |_\\__ \\                       \r\n |____/             \\_____|_|  \\___|\\__,_|_|\\__|___/                       \r\n   / /             |  ____|    (_) |                                       \r\n  / /_    ______   | |__  __  ___| |_                                      \r\n | '_ \\  |______|  |  __| \\ \\/ / | __|                                     \r\n | (_) |           | |____ >  <| | |_                                      \r\n  \\___/            |______/_/\\_\\_|\\__|                                     \r\n                                                                           \r\n                                                                           \r\n", 0);
 
 int menu_opt = Convert.ToInt32(Console.ReadLine());
 
@@ -27,32 +33,32 @@ switch (menu_opt)
 void NewGameStart()
 {
     Console.Clear();
-    Console.WriteLine("What's your name?");
+    Print("What's your name?");
     Console.WriteLine();
     playerName = Console.ReadLine();
     Console.WriteLine();
 
 
 
-    Console.WriteLine("You woke up in your mother's house.");
-    Console.WriteLine("It was a strangely quiet morning in Berlin.");
-    Console.WriteLine("A few days ago the doctor took your mom to the hospital, you should pay her a visit soon.");
+    Print("You woke up in your mother's house.");
+    Print("It was a strangely quiet morning in Berlin.");
+    Print("A few days ago the doctor took your mom to the hospital, you should pay her a visit soon.");
     Console.WriteLine();
-    Console.WriteLine("Doc told us not to go but who is he to tell us what to do.");
+    Print("Doc told us not to go but who is he to tell us what to do.");
     Tutorial();
 }
 
 void Tutorial()
 {
-    Console.WriteLine("Get bro to come w u");
-    Console.WriteLine("Y/N");
+    Print("Get bro to come w u");
+    Print("Y/N");
     string choice = Console.ReadLine();
     if (choice.ToUpper() == "Y")
     {
-        Console.WriteLine("Bro not home anyway");
+        Print("Bro not home anyway");
 
-        Console.WriteLine("Milo (your friend) comes over and offers you he'll come with you of you help him get some mushrooms");
-        Console.WriteLine("Will you help him? (Y/N)");
+        Print("Milo (your friend) comes over and offers you he'll come with you of you help him get some mushrooms");
+        Print("Will you help him? (Y/N)");
         choice = Console.ReadLine();
 
         if (choice.ToUpper() == "Y")
@@ -66,8 +72,8 @@ void Tutorial()
     }
     else
     {
-        Console.WriteLine("Milo (your friend) comes over and offers you he'll come with you of you help him get some mushrooms");
-        Console.WriteLine("Will you help him? (Y/N)");
+        Print("Milo (your friend) comes over and offers you he'll come with you of you help him get some mushrooms");
+        Print("Will you help him? (Y/N)");
         choice = Console.ReadLine();
 
         if (choice.ToUpper() == "Y")
@@ -84,6 +90,62 @@ void MushroomMiniGame()
     {
 
     }
+void Print(string text, int speed=60)
+{
+    foreach (char c in text)
+    {
+        Console.Write(c);
+        Thread.Sleep(speed);
+    }
+    
+}
+void Battle(string type)
+{
+    Print("Our homeland is in danger. Everything is awful.");
+    Print("Player Turn");
+    Console.WriteLine("[1] - Attack \n [2] - Items \n [3] - Talk \n [4] - Flee \n");
+
+    string battleChoice = Console.ReadLine();
+    Random rng = new Random();
+
+    int enemyHealth = rng.Next(1, 200);
+    int enemyDamage = rng.Next(5, 20);
+    string enemyName = enemyNames[rng.Next(10)];
+
+    switch (battleChoice)
+    {
+        case "1":
+            
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            Console.WriteLine("Goodbye!");
+            Print("And you ran away like a coward");
+            break;
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 
